63454006a13831460172a75a07b6a9616b4b5f26
TommyFiga <tomasfvfx@outlook.com>
2024-03-19 09:38:15 -0400

    chore: Adição do exercício 2 com a sua respetiva makefile

---
+++ ex2/prog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    printf("MSG 1\n");
+
+    for (int i = 0; i < 3; i++) {
+        if (fork() == 0) {
+            printf("MSG 2\n");
+        }
+    }
+    
+    printf("MSG 3\n");
+
+    return 0;
+}
