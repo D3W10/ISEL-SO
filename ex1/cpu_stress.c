7fd37e6895ebfe79ad72058980648bba7a04f87f
D3W10 <danyribeironunes15@gmail.com>
2024-03-19 15:14:13 +0000

    feat: Continue working on the exercise 1

--- ex1/cpu_stress.c
+++ ex1/cpu_stress.c
@@ -1,3 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 void process_work(long niter) {
     for (long i = 0; i < niter; i++) {
         sqrt(rand());
@@ -5,6 +12,25 @@ void process_work(long niter) {
 }
 
 int main(int argc, char *argv[]) {
-    process_work(1e9);
+    if (argc < 2) {
+        printf("Usage: %s <number_of_processes>\n", argv[0]);
+        exit(1);
+    }
+
+    for (int i = 0; i < atoi(argv[1]); i++)
+    {
+        pid_t pId = fork();
+        if (pId < 0) {
+            perror("fork");
+            return -1;
+        }
+        else if (pId != 0) {
+            process_work(1e9);
+            return 0;
+        }
+    }
+
+    wait(12);
+
     return 0;
 }
