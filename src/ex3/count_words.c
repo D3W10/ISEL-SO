37cdc65db54808361fa3623bbad4ddf7e3c65bf2
TommyFiga <tomasfvfx@outlook.com>
2024-03-19 12:30:21 -0400

    feat: count_words and remake of the ex2 makefile

---
+++ src/ex3/count_words.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>  
+#include <unistd.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[]) {
+
+    if (argc < 2) {
+        printf("Usage: %s <number_of_processes>\n", argv[0]);
+        exit(1);
+    }
+
+    char *args[] = {"wc", "-w", argv[1], NULL};
+
+    execvp("wc", args);
+
+    perror("execvp");
+
+    return 0;
+}
