bfd84bb73967920b0d8e4ff020ac873b8c24d914
D3W10 <danyribeironunes15@gmail.com>
2024-04-16 14:43:27 +0100

    feat: Add exercise 1

--- src/trab2/ex1/pc_info.c
+++ src/trab2/ex1/pc_info.c
@@ -1,3 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
 int main() {
+    int fd[2];
+    pipe(fd);
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        close(fd[0]);
+        dup2(fd[1], STDOUT_FILENO);
+        execl("/usr/bin/lscpu", "lscpu", NULL);
+    }
+    else {
+        close(fd[1]);
+
+        char buffer[1024];
+        ssize_t bytes_read;
+        while ((bytes_read = read(fd[0], buffer, sizeof(buffer))) > 0) {
+            for (int i = 0; i < bytes_read; i++)
+                putchar(toupper(buffer[i]));
+        }
+
+        wait(pid);
+    }
+
     return 0;
 }
