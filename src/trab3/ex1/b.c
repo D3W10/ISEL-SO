2b2ee48d6d8e401b1a881faeae5f4c2dd3e49961
Daniel Nunes <danyribeironunes15@gmail.com>
2024-05-21 15:45:56 +0100

    feat: Add exercise 1 files plus the fixed version of 1a

---
+++ src/trab3/ex1/b.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void *th1(void *arg) {
+    int *pt = (int *)arg;
+
+    for (int i = 0; i < 10000000; ++i)
+        (*pt) += 2;
+
+    return NULL;
+}
+
+void *th2(void *arg) {
+    int *pt = (int *)arg;
+
+    for (int i = 0; i < 10000000; ++i)
+        (*pt) -= 3;
+
+    return NULL;
+}
+
+void *th3(void *arg) {
+    int *pt = (int *)arg;
+
+    for (int i = 0; i < 10000000; ++i)
+        (*pt)++;
+
+    return NULL;
+}
+
+int main() {
+    int count = 0;
+    pthread_t t1, t2, t3;
+    int a = 0, b = 0, c = 0;
+
+    pthread_create(&t1, NULL, th1, &a);
+    pthread_create(&t2, NULL, th2, &b);
+    pthread_create(&t3, NULL, th3, &c);
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    pthread_join(t3, NULL);
+
+    count = a + b + c;
+    printf("Total = %d\n", count);
+}
