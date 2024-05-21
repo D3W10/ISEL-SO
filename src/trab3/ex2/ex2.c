0fdd4423bc0266d06a79c3b96fe9631fe6153bc3
EduardoCosta50861 <duda.louco8@hotmail.com>
2024-05-21 16:23:08 +0100

    feat: Finished ex2.c

--- src/trab3/ex2/ex2.c
+++ src/trab3/ex2/ex2.c
@@ -9,6 +9,7 @@
 typedef struct ChargePoint_t {
     char lugares[MAX_CAPACITY];
     sem_t sLugaresLivres;
+    sem_t sFilaPrioritaria;
     pthread_mutex_t mutex;
     int priority;
 } ChargePoint;
@@ -37,10 +38,60 @@ void freeChargePoint(ChargePoint *point, int lugar) {
     point->lugares[lugar] = 1;
     pthread_mutex_unlock(&point->mutex);
     sem_post(&point->sLugaresLivres);
+
+
+    if(point->priority > 0){
+        sem_post(&point->sFilaPrioritaria);
+    }
 }
 
 int reserveChargePointPriority(ChargePoint *point) {
+    int pos = -1;    
+
+    pthread_mutex_lock(&point->mutex);
+    point->priority++;
+    pthread_mutex_unlock(&point->mutex);
+
+    sem_wait(&point->sFilaPrioritaria);
+
+    pthread_mutex_lock(&point->mutex);
+
+    sem_wait(&point->sLugaresLivres);
+
+    for(int i = 0; i < MAX_CAPACITY; i++){
+        if(point->lugares[i] == 1){
+            pos = i;
+            point->lugares[i] = 0;
+            break;
+        }
+    }
     
+    point->priority--;
+
+    if(point->priority > 0){
+        sem_post(&point->sFilaPrioritaria);
+    }
+
+    pthread_mutex_unlock(&point->mutex);
+
+    return pos;
+}
+
+int main() {
+    ChargePoint chargePoint;
+    sem_init (&chargePoint.sLugaresLivres, 0, MAX_CAPACITY);
+    sem_init (&chargePoint.sFilaPrioritaria, 0, 0);
+    pthread_mutex_init (&chargePoint.mutex, NULL);
     
-    return ;
+    for(int i = 0; i < MAX_CAPACITY; i++){
+        chargePoint.lugares[i] = 1;
+    }
+
+
+    
+    sem_destroy (&chargePoint.sLugaresLivres);
+    sem_destroy (&chargePoint.sFilaPrioritaria);
+    pthread_mutex_destroy (&chargePoint.mutex);
+
+    return 0;
 }
