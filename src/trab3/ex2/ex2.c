09e9f777e5e9ba4b9488c9911092d7d071c54312
Daniel Nunes <danyribeironunes15@gmail.com>
2024-05-31 13:26:03 +0100

    style: Change styling on ex2.c

--- src/trab3/ex2/ex2.c
+++ src/trab3/ex2/ex2.c
@@ -27,7 +27,7 @@ int reserveChargePoint(ChargePoint *point) {
             break;
         }
     }
-    
+
     pthread_mutex_unlock(&point->mutex);
 
     return pos;
@@ -39,38 +39,33 @@ void freeChargePoint(ChargePoint *point, int lugar) {
     pthread_mutex_unlock(&point->mutex);
     sem_post(&point->sLugaresLivres);
 
-
-    if(point->priority > 0){
+    if (point->priority > 0)
         sem_post(&point->sFilaPrioritaria);
-    }
 }
 
 int reserveChargePointPriority(ChargePoint *point) {
-    int pos = -1;    
+    int pos = -1;
 
     pthread_mutex_lock(&point->mutex);
     point->priority++;
     pthread_mutex_unlock(&point->mutex);
 
     sem_wait(&point->sFilaPrioritaria);
-
     pthread_mutex_lock(&point->mutex);
-
     sem_wait(&point->sLugaresLivres);
 
-    for(int i = 0; i < MAX_CAPACITY; i++){
-        if(point->lugares[i] == 1){
+    for (int i = 0; i < MAX_CAPACITY; i++) {
+        if (point->lugares[i] == 1) {
             pos = i;
             point->lugares[i] = 0;
             break;
         }
     }
-    
+
     point->priority--;
 
-    if(point->priority > 0){
+    if (point->priority > 0)
         sem_post(&point->sFilaPrioritaria);
-    }
 
     pthread_mutex_unlock(&point->mutex);
 
@@ -79,19 +74,17 @@ int reserveChargePointPriority(ChargePoint *point) {
 
 int main() {
     ChargePoint chargePoint;
-    sem_init (&chargePoint.sLugaresLivres, 0, MAX_CAPACITY);
-    sem_init (&chargePoint.sFilaPrioritaria, 0, 0);
-    pthread_mutex_init (&chargePoint.mutex, NULL);
-    
-    for(int i = 0; i < MAX_CAPACITY; i++){
+
+    sem_init(&chargePoint.sLugaresLivres, 0, MAX_CAPACITY);
+    sem_init(&chargePoint.sFilaPrioritaria, 0, 0);
+    pthread_mutex_init(&chargePoint.mutex, NULL);
+
+    for (int i = 0; i < MAX_CAPACITY; i++)
         chargePoint.lugares[i] = 1;
-    }
 
-
-    
-    sem_destroy (&chargePoint.sLugaresLivres);
-    sem_destroy (&chargePoint.sFilaPrioritaria);
-    pthread_mutex_destroy (&chargePoint.mutex);
+    sem_destroy(&chargePoint.sLugaresLivres);
+    sem_destroy(&chargePoint.sFilaPrioritaria);
+    pthread_mutex_destroy(&chargePoint.mutex);
 
     return 0;
 }
